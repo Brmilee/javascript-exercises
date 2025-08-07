54cb03b9ddac4bf79112c6489a76ce5357b083c8
Brandon Lee <135774657+EonWizard@users.noreply.github.com>
2025-08-07 23:44:20 +0000

    Update Exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(str) {
+// const arr = str.split('');
+// arr = arr.Array.prototype.reverse();
+// let str = arr.toString();
+// return str;
+// };
+return str.split("").reverse().join("");
 
 // Do not edit below this line
 module.exports = reverseString;
+}
