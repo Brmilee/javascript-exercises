5fefb267ab1e75aa4366aa114514f87f04fa1371
Brandon Lee <135774657+EonWizard@users.noreply.github.com>
2025-08-07 23:30:48 +0000

    Update Exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    
+    
+    // while (num >= 0){
+    //     string += string;
+    // }
+    // return string;
+
+    if (num < 0) return "ERROR";
+    let string = "";
+    for (i = 0; i < num; i++){
+        string += word;
+    }
+    return string;
 
 };
 
